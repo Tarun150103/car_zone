9867d16ca36d7b0f006f0349fc99dfc8c14577c9
TARUN NETHI <tarunteja.n@westagilelabs.in>
2021-09-30 22:28:45 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://young-garden-79468.herokuapp.com/' }
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'taruntejanethi@gmail.com',
+  password:             'Teja@21345678',
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
 end
